a66e7a38256fce533540750ba88bbafb66038910
srsly4 <piechaczek.szymon@gmail.com>
2017-01-09 14:22:34 +0100

    Made game harder

--- src/main/java/game/grabbables/GrabbableManager.java
+++ src/main/java/game/grabbables/GrabbableManager.java
@@ -33,26 +33,28 @@ public class GrabbableManager implements IGameObject {
 
     private void loadWatchers(){
         watchers.clear();
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(2100, 1000), 1500f, 800f));
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(2200, 2000), 1750f, 1000f));
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(3200, 2000), 2222f, 1800f));
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(2100, 1000), 1877f, 2100f));
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(3200, 2500), 1200f, 3400f));
-        watchers.add(new GrabbableWatcher("rocket", randomInterval(2200, 2000), 1800f, 3900f));
-        watchers.add(new GrabbableWatcher("wind", randomInterval(2100, 1000), 1700f, 1500f));
-        watchers.add(new GrabbableWatcher("wind", randomInterval(2300, 2000), 1400f, 2800f));
-        watchers.add(new GrabbableWatcher("wind", randomInterval(2200, 2000), 1200f, 4300f));
-        watchers.add(new GrabbableWatcher("rebound", randomInterval(3500, 3000), 2000f, Hamster.groundPos));
-        watchers.add(new GrabbableWatcher("rebound", randomInterval(3500, 5000), 4500f, Hamster.groundPos));
-        watchers.add(new GrabbableWatcher("bounce", randomInterval(2500, 2000), 2300f, 1400f));
-        watchers.add(new GrabbableWatcher("bounce", randomInterval(3700, 2000), 1450f, 3100f));
-        watchers.add(new GrabbableWatcher("bounce", randomInterval(2500, 2000), 1650f, 2250f));
-        watchers.add(new GrabbableWatcher("super_bounce", randomInterval(4000, 2000), 3450f, 2600f));
-        watchers.add(new GrabbableWatcher("super_bounce", randomInterval(4000, 2000), 2750f, 3600f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(4100, 1000), 1500f, 800f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(4700, 2000), 1750f, 1000f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(4300, 2000), 2750f, 1200f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(5200, 2000), 2222f, 1800f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(4100, 1000), 1877f, 2100f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(5200, 2500), 1200f, 3400f));
+        watchers.add(new GrabbableWatcher("rocket", randomInterval(4200, 2000), 1800f, 3900f));
+        watchers.add(new GrabbableWatcher("wind", randomInterval(4100, 1000), 1700f, 1500f));
+        watchers.add(new GrabbableWatcher("wind", randomInterval(3300, 2000), 1400f, 2800f));
+        watchers.add(new GrabbableWatcher("wind", randomInterval(4200, 2000), 1200f, 4300f));
+        watchers.add(new GrabbableWatcher("rebound", randomInterval(4700, 3000), 2000f, Hamster.groundPos));
+        watchers.add(new GrabbableWatcher("rebound", randomInterval(6200, 3000), 4500f, Hamster.groundPos));
+        watchers.add(new GrabbableWatcher("bounce", randomInterval(4500, 2000), 2300f, 1400f));
+        watchers.add(new GrabbableWatcher("bounce", randomInterval(5700, 2000), 1450f, 3100f));
+        watchers.add(new GrabbableWatcher("bounce", randomInterval(4500, 2000), 1650f, 2250f));
+        watchers.add(new GrabbableWatcher("super_bounce", randomInterval(6000, 2000), 3450f, 2600f));
+        watchers.add(new GrabbableWatcher("super_bounce", randomInterval(7000, 2000), 2750f, 3600f));
 
         watchers.add(new GrabbableWatcher("cloud", randomInterval(2000, 750), 1000, 5600f));
         watchers.add(new GrabbableWatcher("cloud", randomInterval(2300, 1000), 1340, 6000f));
         watchers.add(new GrabbableWatcher("cloud", randomInterval(2700, 1000), 640, 5750f));
+        watchers.add(new GrabbableWatcher("cloud", randomInterval(3400, 1000), 1200, 6100f));
     }
 
     public void reset(){
